c0d649cb16ce7b965e7ca0087362a88d60511d9a
amine ziad ounnoughene <86957029+amine-ziad-ounnoughene@users.noreply.github.com>
2022-01-27 19:54:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,27 @@ import requests
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 @app.get("/weather")
 def weather():
   city = "oran"
