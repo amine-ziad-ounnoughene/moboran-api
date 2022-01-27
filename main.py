c790e2f9b54f8a936527c045c4213770d00bd0b3
amine ziad ounnoughene <86957029+amine-ziad-ounnoughene@users.noreply.github.com>
2022-01-27 11:00:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def forcast():
 @app.get('/salat/{day}/')
 def salat(day):
   import requests
-   response = requests.get("http://api.aladhan.com/v1/calendar?latitude=35.6976541&longitude=-0.6337376&method=5&month=1&year=2022")
-   for i in list(response.json()["data"]):
+  response = requests.get("http://api.aladhan.com/v1/calendar?latitude=35.6976541&longitude=-0.6337376&method=5&month=1&year=2022")
+  for i in list(response.json()["data"]):
       if i["date"]["readable"][:2] == day:
           return i["timings"]
