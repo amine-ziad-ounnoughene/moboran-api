f430f1a104ace6607be1404f78f05f8f11eb166b
amine ziad ounnoughene <86957029+amine-ziad-ounnoughene@users.noreply.github.com>
2022-01-27 20:28:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-#origins = ["http://localhost.tiangolo.com","https://localhost.tiangolo.com","http://localhost","http://localhost:8080"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
 
 
 
