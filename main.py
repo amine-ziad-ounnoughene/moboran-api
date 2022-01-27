09f19dfda65ac60f3b947b29a73616d7ffd71a40
amine ziad ounnoughene <86957029+amine-ziad-ounnoughene@users.noreply.github.com>
2022-01-27 22:53:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def weather():
   if x["cod"] != "404":
     return JSONResponse(content=jsonable_encoder({"lat":dict(x["coord"])['lat'],"lon":dict(x["coord"])['lon']
     ,"description":dict(x["weather"][0])['description'],"icon":"https://openweathermap.org/img/wn/" + str(dict(x["weather"][0])['icon'] ) + "@2x.png",
-    "temp-celsius":round(dict(x['main'])['temp']-273),"humidity_%":dict(x['main'])['humidity'],
-    "wind-speed-m/s":dict(x["wind"])["speed"] , "pressure" : dict(response.json())["main"]['pressure']}))
+    "temp":round(dict(x['main'])['temp']-273),"humidity_%":dict(x['main'])['humidity'],
+    "wind":dict(x["wind"])["speed"] , "pressure" : dict(response.json())["main"]['pressure']}))
 @app.get("/covid")
 def covid():
   url = "https://api.corona-dz.live/country/latest"
